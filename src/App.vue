e455f96bec21743e3bcded1a60bf77358ac96dbd
30namp <77108679+30namp@users.noreply.github.com>
2022-09-01 18:27:25 +0430

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,134 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainNavbar class="z-20" />
+  <div class="page-container light-theme px-20 w-full min-h-screen">
+    <div class="top-controll z-10 mt-32 w-full flex flex-row justify-between items-start">
+      <div class="search-box light-theme relative h-16 w-[30rem] rounded-md overflow-hidden">
+        <input type="text" name="search-input" id="search-input" class="light-theme w-full h-full top-0 left-0 right-0 bottom-0 outline-none pl-20 pr-4" placeholder="Search for a country...">
+        <i class="bi bi-search absolute left-0 ml-[2.2rem] text-xl top-[50%]"></i>
+      </div>
+      <div class="filter-box light-theme relative h-16 w-64 rounded-md">
+        <div class="select light-theme cursor-pointer h-16 w-full flex justify-between items-center">
+          <p class="light-theme ml-6">Filter by Region</p>
+          <i class="bi bi-chevron-down mr-6"></i>
+        </div>
+        <div v-show="filterBoxIsOpen" class="options light-theme absolute top-[4.3rem] py-2 w-full flex flex-col justify-start items-center rounded-md shadow-md">
+          <button v-for:="(region, index) in regions" @click="toggleSelectRegion(index)" class="light-theme pl-6 w-full text-left py-1 hover:bg-gray-100 transition duration-200" :style="[region.selected ? 'color: rgb(34 213 44);' : '']">{{ region.name }}</button>
+        </div>
+      </div>
+    </div>
+    <div class="countries mt-12 w-full grid grid-cols-4 gap-16 mb-12">
+      <div v-for:="country in countries" class="country-card light-theme rounded-lg overflow-hidden">
+        <img :src="country.flags.svg" alt="mt" loading="lazy" class="w-full">
+        <p class="country-name text-xl font-extrabold w-full px-6 mt-6">{{ country.name.common }}</p>
+        <p class="country-population text-base font-semibold w-full px-6 mt-2">Population: <span class="font-light"> {{ country.population }}</span></p>
+        <p class="country-region text-base font-semibold w-full px-6 mt-1">Region: <span class="font-light"> {{ country.region }}</span></p>
+        <p class="country-capital text-base font-semibold w-full px-6 mt-1 mb-12">Capital: <span class="font-light"> {{ country.capital[0] }}</span></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainNavbar from './components/MainNavbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MainNavbar
+  },
+  data() {
+    return {
+      filterBoxIsOpen: 0,
+      regions: [
+        {
+          name: 'Africa',
+          selected: false
+        },
+        {
+          name: 'Americas',
+          selected: false
+        },
+        {
+          name: 'Asia',
+          selected: false
+        },
+        {
+          name: 'Europe',
+          selected: false
+        },
+        {
+          name: 'Oceania',
+          selected: false
+        },
+      ],
+      countries: [
+
+      ],
+    }
+  },
+  methods: {
+    filterBoxOptionsToggleHandler()
+    {
+      document.querySelector('body').addEventListener('click', (event) => {
+        if(document.querySelector('.filter-box > .select').contains(event.target))
+        {
+          this.filterBoxIsOpen = 1 - this.filterBoxIsOpen;
+        }
+        else
+        {
+          this.filterBoxIsOpen = 0;
+        }
+      });
+    },
+    toggleSelectRegion(index)
+    {
+      this.regions[index]['selected'] = 1 - this.regions[index]['selected'];
+    },
+    getCountries()
+    {
+      this.countries = [
+        
+      ];
+    }
+  },
+  mounted() {
+    this.filterBoxOptionsToggleHandler(),
+    this.getCountries()
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<style scoped>
+
+.search-box {
+  background: var(--element-color);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.08);
+}
+
+.search-box > i {
+  color: var(--input-color);
+  transform: translate(0, -50%);
+}
+
+.filter-box {
+  background: var(--element-color);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.08);
+}
+
+.filter-box > .options {
+  background: var(--element-color);
+}
+
+#search-input::-webkit-input-placeholder {
+  color: var(--input-color);
+  font-family: 'Nunito Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+.country-card {
+  background: var(--element-color);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.08);
+}
+
 </style>
---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="light-theme fixed top-0 w-full h-20 shadow-md flex items-center justify-between">
+        <p class="light-theme text-2xl font-extrabold ml-20">Where in the world?</p>
+        <button class="light-theme text-base font-semibold mr-20">
+            <i class="bi bi-moon mr-0.5"></i>
+            Dark Mode
+        </button>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    name: 'MainNavbar'
+}
+
+</script>
+
+<style scoped>
+
+nav {
+    background: var(--element-color);
+}
+
+</style>
