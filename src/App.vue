41243f412a6d2e3712d3eae1c371aba29816da3c
30namp <77108679+30namp@users.noreply.github.com>
2022-09-03 03:13:04 +0430

    finall creation

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="dark-theme-background fixed w-0 h-full" style="top: 50%;left: 50%;transform:translate(-50%, -50%);background: hsl(207, 26%, 17%);transition: 500ms;"></div>
   <MainNavbar class="z-20" />
-  <div class="page-container light-theme px-20 w-full min-h-screen">
-    <div v-show="infoPageIsOpen === false" class="top-controll z-10 mt-32 w-full flex flex-row justify-between items-start">
-      <div class="search-box light-theme relative h-16 w-[30rem] rounded-md overflow-hidden">
-        <input type="text" name="search-input" id="search-input" v-model="searchValue" @change="updateResultBySearch" class="light-theme w-full h-full top-0 left-0 right-0 bottom-0 outline-none pl-20 pr-4" style="background: transparent;" placeholder="Search for a country...">
-        <i class="bi bi-search absolute left-0 ml-[2.2rem] text-xl top-[50%]"></i>
+  <div class="page-container light-theme px-6 sm:px-20 w-full min-h-screen">
+    <div v-show="infoPageIsOpen === false" class="top-controll z-10 mt-24 sm:mt-32 w-full flex flex-row justify-between items-start flex-wrap sm:flex-nowrap">
+      <div class="search-box light-theme relative h-12 sm:h-16 w-[30rem] rounded-md overflow-hidden">
+        <input type="text" name="search-input" id="search-input" v-model="searchValue" @change="updateResultBySearch" class="light-theme w-full h-full top-0 left-0 right-0 bottom-0 outline-none pl-14 sm:pl-20 pr-4 text-sm sm:text-base" style="background: transparent;" placeholder="Search for a country...">
+        <i class="bi bi-search absolute left-0 ml-6 sm:ml-[2.2rem] text-base sm:text-xl top-[50%]"></i>
       </div>
-      <div class="filter-box light-theme relative h-16 w-64 rounded-md">
-        <div class="select light-theme cursor-pointer h-16 w-full flex justify-between items-center">
-          <p class="light-theme ml-6">Filter by Region</p>
-          <i class="bi bi-chevron-down mr-6"></i>
+      <div class="filter-box light-theme relative h-12 sm:h-16 w-48 sm:w-64 rounded-md mt-10 sm:mt-0">
+        <div class="select light-theme cursor-pointer h-12 sm:h-16 w-full flex justify-between items-center">
+          <p class="light-theme ml-6 text-sm sm:text-base">Filter by Region</p>
+          <i class="bi bi-chevron-down mr-6 text-sm sm:text-base"></i>
         </div>
-        <div v-show="filterBoxIsOpen" class="options light-theme absolute top-[4.3rem] py-2 w-full flex flex-col justify-start items-center rounded-md shadow-md">
-          <button v-for:="(region, index) in regions" @click="toggleSelectRegion(index)" class="region-selector-option light-theme pl-6 w-full text-left py-1 transition duration-200" :style="[region.selected ? 'color: rgb(34 213 44);' : '']">{{ region.name }}</button>
+        <div v-show="filterBoxIsOpen" class="options light-theme absolute top-[3.4rem] sm:top-[4.3rem] py-2 w-full flex flex-col justify-start items-center rounded-md shadow-md">
+          <button v-for:="(region, index) in regions" @click="toggleSelectRegion(index)" class="region-selector-option light-theme pl-6 w-full text-left py-1 transition duration-200 text-sm sm:text-base" :style="[region.selected ? 'color: rgb(34 213 44);' : '']">{{ region.name }}</button>
         </div>
       </div>
     </div>
-    <div v-show="infoPageIsOpen === false" class="countries mt-12 w-full grid grid-cols-4 gap-16">
-      <div v-for:="country in countries" class="country-card cursor-pointer light-theme rounded-lg overflow-hidden" @click="openCountryInfo(country)">
+    <div v-show="infoPageIsOpen === false" class="countries mt-12 w-full grid grid-cols-1 sm:grid-cols-4 gap-16">
+      <div v-for:="country in countries" class="country-card mx-8 cursor-pointer light-theme rounded-lg overflow-hidden" @click="openCountryInfo(country)">
         <img :src="country.flags.png" alt="mt" loading="lazy" class="w-full h-[190px]">
-        <p class="country-name text-xl font-extrabold w-full px-6 mt-6">{{ country.name.common }}</p>
-        <p class="country-population text-base font-extrabold w-full px-6 mt-2">Population: <span class="font-semibold"> {{ country.population }}</span></p>
-        <p class="country-region text-base font-extrabold w-full px-6 mt-1">Region: <span class="font-semibold"> {{ country.region }}</span></p>
-        <p class="country-capital text-base font-extrabold w-full px-6 mt-1 mb-12">Capital: <span class="font-semibold"> {{ country.capital[0] }}</span></p>
+        <p class="country-name text-xl font-semibold sm:font-extrabold w-full px-6 mt-6">{{ country.name.common }}</p>
+        <p class="country-population text-base font-semibold sm:font-extrabold w-full px-6 mt-2">Population: <span class="font-light sm:font-semibold"> {{ country.population }}</span></p>
+        <p class="country-region text-base font-semibold sm:font-extrabold w-full px-6 mt-1">Region: <span class="font-light sm:font-semibold"> {{ country.region }}</span></p>
+        <p class="country-capital text-base font-semibold sm:font-extrabold w-full px-6 mt-1 mb-12">Capital: <span class="font-light sm:font-semibold"> {{ country.capital[0] }}</span></p>
       </div>
     </div>
     <div v-show="infoPageIsOpen === false && countries.length" class="country-stack-pages mt-12 w-full grid-cols-3 gap-16 mb-12">
@@ -34,35 +34,47 @@
       </div>
     </div>
 
-    <div v-show="infoPageIsOpen === true" class="back-btn-container mt-44 w-full flex flex-row justify-start items-start">
-      <button @click="closeInfoPage" class="back-btn light-theme relative h-12 w-44 rounded-md overflow-hidden shadow-md flex justify-center items-center gap-3" style="background: var(--element-color);">
-        <i class="bi bi-arrow-left text-2xl"></i>
-        <p class="light-theme text-xl font-semibold">Back</p>
+    <div v-show="infoPageIsOpen === true" class="back-btn-container mt-32 sm:mt-44 w-full flex flex-row justify-start items-start">
+      <button @click="closeInfoPage" class="back-btn light-theme relative h-8 sm:h-12 w-28 sm:w-44 rounded-sm sm:rounded-md overflow-hidden shadow-md flex justify-center items-center gap-3" style="background: var(--element-color);">
+        <i class="bi bi-arrow-left text-lg sm:text-2xl"></i>
+        <p class="light-theme text-sm sm:text-xl font-semibold">Back</p>
       </button>
     </div>
-    <div v-show="infoPageIsOpen === true" class="country-contents mt-20 w-full flex flex-row justify-between items-start">
-      <div class="img w-[36.5rem]">
+    <div v-show="infoPageIsOpen === true" class="country-contents mt-20 w-full flex flex-col sm:flex-row justify-start sm:justify-between items-center sm:items-start gap-8 sm:gap-0">
+      <div class="img w-full sm:w-[36.5rem]">
         <img :src="selectedCountry.flags.png" :alt="selectedCountry.name.common + ' flag'" class="w-full">
       </div>
-      <div class="country-content w-[36.5rem] flex flex-col justify-start items-start">
+      <div class="country-content w-full sm:w-[36.5rem] flex flex-col justify-start items-start">
         <p class="text-3xl mt-12 font-extrabold">{{ selectedCountry.name.common }}</p>
-        <div class="country-info  w-full mt-8 grid grid-cols-2 gap-y-2 gap-x-4">
-          <p class="light-theme text-left text-base font-extrabold">Native Name: <span class="font-semibold">{{ selectedCountry.name.nativeName[Object.keys(selectedCountry.name.nativeName)[0]].common }}</span></p>
-          <p class="light-theme text-left text-base font-extrabold">Top Level Domain: <span class="font-semibold">{{ selectedCountry.tld[0] }}</span></p>
-          <p class="light-theme text-left text-base font-extrabold">Population: <span class="font-semibold">{{ selectedCountry.population }}</span></p>
-          <p class="light-theme text-left text-base font-extrabold">Currencies: <span class="font-semibold">{{ selectedCountry.currencies }}</span></p>
-          <p class="light-theme text-left text-base font-extrabold">Region: <span class="font-semibold">{{ selectedCountry.region }}</span></p>
-          <p class="light-theme text-left text-base font-extrabold">Languages: <span class="font-semibold">{{ selectedCountry.languages }}</span></p>
-          <p class="light-theme text-left text-base font-extrabold">Sub Region: <span class="font-semibold">{{ selectedCountry.subregion }}</span></p>
-          <p class="light-theme text-left text-base font-extrabold"> <span class="font-semibold"></span></p>
-          <p class="light-theme text-left text-base font-extrabold">Capital: <span class="font-semibold">{{ selectedCountry.capital.join(', ') }}</span></p>
+        <div class="country-info w-full mt-8 grid grid-cols-2 gap-y-2 sm:gap-x-4">
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1">Native Name: <span class="font-semibold">{{ selectedCountry.name.nativeName[Object.keys(selectedCountry.name.nativeName)[0]].common }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1 hidden sm:visible">Top Level Domain: <span class="font-semibold">{{ selectedCountry.tld[0] }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1">Population: <span class="font-semibold">{{ selectedCountry.population }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1 hidden sm:visible">Currencies: <span class="font-semibold">{{ selectedCountry.currencies }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1">Region: <span class="font-semibold">{{ selectedCountry.region }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1 hidden sm:visible">Languages: <span class="font-semibold">{{ selectedCountry.languages }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1">Sub Region: <span class="font-semibold">{{ selectedCountry.subregion }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1 hidden sm:visible"> <span class="font-semibold"></span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1">Capital: <span class="font-semibold">{{ selectedCountry.capital.join(', ') }}</span></p>
+          
           <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
           <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
           <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
           <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
           <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
-          <div class="light-theme col-span-2 flex flex-row justify-start items-start gap-2 flex-wrap">
-            <p class="light-theme text-left text-base font-extrabold h-8 flex justify-center items-center">Border Countries:</p> 
+
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1 visible sm:hidden">Top Level Domain: <span class="font-semibold">{{ selectedCountry.tld[0] }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1 visible sm:hidden">Currencies: <span class="font-semibold">{{ selectedCountry.currencies }}</span></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2 sm:col-span-1 visible sm:hidden">Languages: <span class="font-semibold">{{ selectedCountry.languages }}</span></p>
+          
+          <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
+          <p class="light-theme text-left text-base font-extrabold col-span-2"></p>
+
+          <div class="light-theme col-span-2 flex flex-row justify-start items-start gap-2 flex-wrap mb-8 sm:mb-0">
+            <p class="light-theme text-left text-base font-extrabold h-8 flex justify-start sm:justify-center items-center w-full sm:w-auto">Border Countries:</p> 
             <span v-for:="borderCountry in selectedCountry.borders" class="font-semibold px-8 h-8 shadow-md flex justify-center items-center rounded-sm" style="background: var(--element-color);">
               {{ borderCountry }}
             </span>
@@ -257,6 +269,8 @@ export default {
         this.nowCountryStackPage = this.allCountries.length;
       }
       this.countries = this.allCountries[this.nowCountryStackPage - 1];
+
+      this.scrollToTop();
     },
     openCountryInfo(country = this.countries[0])
     {
--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="light-theme fixed top-0 w-full h-20 shadow-md flex items-center justify-between">
-        <p class="light-theme text-2xl font-extrabold ml-20">Where in the world?</p>
-        <button @click="switchTheme" class="switch-theme-btn light-theme text-base font-semibold mr-20">
+        <p class="light-theme text-md sm:text-2xl font-extrabold ml-6 sm:ml-20">Where in the world?</p>
+        <button @click="switchTheme" class="switch-theme-btn light-theme text-sm sm:text-base font-semibold mr-6 sm:mr-20">
             <i class="bi bi-moon mr-0.5"></i>
             Dark Mode
         </button>
